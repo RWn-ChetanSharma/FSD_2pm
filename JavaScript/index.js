3260fb94b16584ba401118feb82eb671d7df5fe1
RWn-ChetanSharma <rwa5.chetan.sha@gmail.com>
2024-06-20 14:02:46 +0530

    Lecture Data

---
+++ JavaScript/index.js
@@ -0,0 +1,9 @@
+console.log("Namaste Bharat");
+let a = 20;
+// // console.log(`a : ${a} and a++ : ${a++}`); // Post Increment or Post Workout
+// // console.log(`a : ${a} and a++ : ${a++}`);
+// console.log(`a : ${a} and ++a : ${++a} and --a : ${--a}`); // Pre Increment or Pre Workout
+// // console.log(`a : ${a} and --a : ${--a}`);
+// // console.log(`a : ${a} and a++ : ${a++}`);
+// console.log(`a : ${a}`);
+document.table(["apples", "oranges", "bananas"]);
