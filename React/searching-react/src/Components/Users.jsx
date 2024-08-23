026f61e9e9c67a116dda1e95cc357b48a28ac074
RWn-ChetanSharma <rwa5.chetan.sha@gmail.com>
2024-08-23 11:07:22 +0530

    Lecture Data

---
+++ React/searching-react/src/Components/Users.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+
+function Users() {
+  const [user, setUser] = useState([]);
+  const [term, setTerm] = useState("");
+
+  //   Api Calling
+
+  useEffect(() => {
+    async function fetchData() {
+      const fetchApi = await fetch(
+        `https://jsonplaceholder.typicode.com/users`
+      );
+      console.log(fetchApi);
+      const data = await fetchApi.json();
+      console.log(data);
+      setUser(data);
+    }
+    fetchData();
+  }, []);
+
+  //   render users
+
+//   const renderUser = user.map((e) => (
+//     <div key={e.id}>
+//       <p>
+//         <strong>{e.name}</strong>
+//       </p>
+//     </div>
+//   ));
+
+  //   Searching
+
+  const searchingUser = user
+    .filter(({ name }) => {
+      return name.indexOf(term) >= 0;
+    })
+    .map((e) => (
+      <div key={e.id}>
+        <p>
+          <strong>{e.name}</strong>
+        </p>
+      </div>
+    ));
+
+  return (
+    <>
+      <div className="container text-bg-danger text-center p-5">
+        <h1>Users</h1>
+        <input type="text" value={term} onChange={(e) => setTerm(e.target.value)}/>
+        {searchingUser}
+        {/* {renderUser} */}
+      </div>
+    </>
+  );
+}
+
+export default Users;
