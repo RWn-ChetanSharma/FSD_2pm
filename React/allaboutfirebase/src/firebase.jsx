ef8faaa6f30626ca10ce80c83b45111a9cfe5e5a
RWn-ChetanSharma <rwa5.chetan.sha@gmail.com>
2024-09-30 12:06:21 +0530

    Lecture Data

--- React/allaboutfirebase/src/firebase.jsx
+++ React/allaboutfirebase/src/firebase.jsx
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_FIREBASE__AUTH_DOMAIN,
-  projectId: process.env.REACT_FIREBASE__PROJECT_ID,
-  storageBucket: process.env.REACT_FIREBASE__STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_FIREBASE__MESSAGING_SENDER_ID,
-  appId: process.env.REACT_FIREBASE__APP_ID,
-  databaseURL: process.env.REACT_FIREBASE__DATABASE_URL,
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
 };
 
 export const app = initializeApp(firebaseConfig);
